02a818bb37546a13a2061212afdcc754acb7d091
Cleancookie <pokemon999101@hotmail.co.uk>
2021-03-14 18:53:48 +0000

    🚧 Starting to writ eevent listeners for FE

--- server/services/SocketService.js
+++ server/services/SocketService.js
@@ -7,22 +7,27 @@ let state = {
 };
 
 io.on('connection', (socket) => {
-    console.log(`✨ new user playerId = ${socket.id}`);
+    console.log(`✨ new user (${socket.id})`);
 
     // Init new player on server side
-    state[socket.id] = {};
+    state.players[socket.id] = {};
+    socket.join('MainRoom');
 
     // Init player on client side
     socket.emit('fullState', state);
 
     // Socket event listeners
+    socket.on('disconnect', () => {
+        delete state.players[socket.id];
+    });
+
     socket.on('getFullState', (data) => {
         return socket.emit('fullState', state);
     });
 
     socket.on('move', (data) => {
         // Check there is an x and y
-        if (!(x in data) || !(y in data)) {
+        if (!('x' in data) || !('y' in data)) {
             return;
         }
 
@@ -31,8 +36,9 @@ io.on('connection', (socket) => {
 });
 
 async function setPosition(playerId, x, y) {
-    state[playerId].x = x;
-    state[playerId].y = y;
+    console.log(`🏃 User(${playerId}) moved to [${x}, ${y}]`);
+    state.players[playerId].x = x;
+    state.players[playerId].y = y;
 }
 
 module.exports = io;
